bb6809a9149903af3f8663fb9286f458cd022db0
José Simões <jose.simoes@eclo.solutions>
2021-11-25 12:45:11 +0000

    Work CI-CD
    - Update azure pipeline yaml.
    - Fix nuspec.
    - Add nuget assets.
    - Update projects to migrate to VS2022.
    - Fix typos.
    - Rename files in repo.
    
    ***NO_CI***

--- Windows.Storage.Streams/Properties/AssemblyInfo.cs
+++ Windows.Storage.Streams/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Windows.Storage.Streams")]
+[assembly: AssemblyTitle("nanoFramework.Windows.Storage.Streams")]
 [assembly: AssemblyCompany("nanoFramework Contributors")]
-[assembly: AssemblyProduct("Windows.Storage.Streams")]
+[assembly: AssemblyProduct("nanoFramework.Windows.Storage.Streams")]
 [assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
 
 ////////////////////////////////////////////////////////////////
